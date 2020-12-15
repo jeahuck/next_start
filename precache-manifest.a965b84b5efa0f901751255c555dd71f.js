ab65921e0234d9aa9f66dcbe6378382e94cd6f8c
qwpoaslkal <qwpoaslkal@naver.com>
2020-12-15 09:55:37 +0900

    양재혁
    빌드한것들 올리기

---
+++ precache-manifest.a965b84b5efa0f901751255c555dd71f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0bbd08a847dd7b97f8b87046a78cbc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9495af168524ebd02c7b",
+    "url": "/static/js/2.c6c5c59f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c6c5c59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d0a48a87d9527c7ff9",
+    "url": "/static/js/main.5aae4441.chunk.js"
+  },
+  {
+    "revision": "d7b7897ef76db7d994f2",
+    "url": "/static/js/runtime-main.e0a17747.js"
+  }
+]);
