3377236fa89469b0c9709b2ed8edb5d46225e1eb
jeahuck <qwpoaslkal@naver.com>
2022-11-10 17:39:06 +0900

    [양재혁] next.js / tailwindcss / lottieFiles 적용
    next.js
    처음 실행해보지만 nuxt랑 다를것이 없다?
    리액트는 보기만함 jsx 신기함
    
    tailwindcss
    이렇게 간단하게 ui틀을 잡을수있다니 .. 재미있느 기능
    
    lottieFiles
    애니메이션을 json 가져와서 재미있는 기술을 쓰네

---
+++ yang_next/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+import Hero from '../components/home/hero'
+
+export default function Home() {
+  return (
+    <Layout>
+      <Head>
+        <title>포트폴리오</title>
+        <meta name="description" content="처음으로 시작하는 포트폴리오" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section className="fiex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <Hero />
+          </div>
+      </section>
+    </Layout>
+  )
+}
