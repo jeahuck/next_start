eab1dbe01f1145c1f74467623594838997e13f39
jeahuck <qwpoaslkal@naver.com>
2022-12-06 11:19:42 +0900

    [양재혁] 제거

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== "production";
-const repository = "";
+// const debug = process.env.NODE_ENV !== "production";
+// const repository = "";
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  assetPrefix: "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
 }
 
