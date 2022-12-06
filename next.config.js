93748be98e30f5b7a8e2fc78a5c798d1e08ba094
jeahuck <qwpoaslkal@naver.com>
2022-12-06 11:22:34 +0900

    [양재혁] 마지막수정..

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-// const debug = process.env.NODE_ENV !== "production";
-// const repository = "";
+const debug = process.env.NODE_ENV !== "production";
+const repository = "next_start/";
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: "", // production 일때 prefix 경로
+  assetPrefix: !debug ? repository : "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
 }
 
