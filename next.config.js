1b78e169d683512b928b0c85d4d0d1282af6e298
jeahuck <qwpoaslkal@naver.com>
2022-12-06 11:24:18 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const debug = process.env.NODE_ENV !== "production";
-const repository = "next_start/";
+const repository = "/next_start/";
 
 const nextConfig = {
   reactStrictMode: true,
