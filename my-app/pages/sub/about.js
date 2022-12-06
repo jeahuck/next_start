7dd93baec96f0ff034e09fdf3ceb338b3264a2b8
jeahuck <qwpoaslkal@naver.com>
2022-12-06 09:33:30 +0900

    [양재혁] 파일구조 변경

--- my-app/pages/sub/about.js
+++
@@ -1,6 +0,0 @@
-export default About => {
-    return <>
-        <h1>/pages/sub/about.js</h1>
-        <a href="/">/pages/index/js</a>
-    </>
-}
--- my-app/pages/sub/index.js
+++
@@ -1,6 +0,0 @@
-export default SubIndex => {
-    return <>
-        <h1>/pages/sub/index.js</h1>
-        <a href="/">/pages/index/js</a>
-    </>
-}
